3af2b6b87982a9cceac1b103f9d2e1ea6315eb02
EmelyAuleitner <gueemely@gmail.com>
2025-12-10 13:18:32 +0100

    tutorial und story

---
+++ Assets/MyGame/Emely/Scripts/TutorialManager.cs
@@ -0,0 +1,67 @@
+﻿
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public Text textFeld;         
+    public string[] texte;
+    private int aktuellerIndex = 0;
+    private bool fertig = false;
+    public GameObject szeneObj;
+    public GameObject[] bilder;
+
+    void Start()
+    {
+        if (texte.Length > 0)
+        {
+            textFeld.text = texte[0];
+        }
+    }
+
+    void Update()
+
+    {
+        if (fertig) return;
+
+       
+        if (Input.anyKeyDown)
+        {
+            
+            aktuellerIndex++;
+
+            
+            if (aktuellerIndex >= texte.Length)
+            {
+                BeendeTutorial();
+                return;
+            }
+            textFeld.text = texte[aktuellerIndex];
+        }
+    }
+
+    void BeendeTutorial()
+    {
+        fertig = true;
+
+        
+        if (textFeld != null)
+            Destroy(textFeld.gameObject);
+
+        if (szeneObj != null)
+            Destroy(szeneObj);
+
+        AktiviereBilder();
+    }
+
+    void AktiviereBilder()
+    {
+        foreach (GameObject bild in bilder)
+        {
+            if (bild != null)
+                bild.SetActive(true);
+        }
+  
+    }
+}
+
