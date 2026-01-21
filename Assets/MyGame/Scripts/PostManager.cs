b7cda79ead27ed35bcc2ec1afa57ca75aeb7d68d
Jana123tt <jana.winnerroider08@htl-salzburg.ac.at>
2026-01-21 11:57:00 +0100

    Update PostManager.cs

--- Assets/MyGame/Scripts/PostManager.cs
+++ Assets/MyGame/Scripts/PostManager.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class PostManager : MonoBehaviour
 {
@@ -10,39 +7,61 @@ public class PostManager : MonoBehaviour
     public GameObject postPanel3;
     public GameObject postPanel4;
 
-    public void Start()
+    void Start()
     {
-        postPanel1.SetActive(false);
-        postPanel2.SetActive(false); 
-        postPanel3.SetActive(false);
-        postPanel4.SetActive(false);
+        HideAllPosts();
     }
+
     public void ReturnButton()
+    {
+        HideAllPosts();
+    }
+
+    void HideAllPosts()
     {
         postPanel1.SetActive(false);
         postPanel2.SetActive(false);
         postPanel3.SetActive(false);
         postPanel4.SetActive(false);
     }
-    public void ShowPost3()
-    { postPanel3.SetActive(true);}
-    public void ShowPost4()
-    { postPanel4.SetActive(true); }
-    public void HidePost3()
-    { postPanel3.SetActive(false); }
-    public void HidePost4()
-    { postPanel4.SetActive(false); }
-    public void ShowPost1()
-    {  postPanel1.SetActive(true);}
 
-    public void HidePost1()
-    { postPanel1.SetActive(false);}
+    public void ShowPost1()
+    {
+        postPanel1.SetActive(true);
+    }
 
     public void ShowPost2()
-    {postPanel2.SetActive(true);}
+    {
+        postPanel2.SetActive(true);
+    }
+
+    public void ShowPost3()
+    {
+        postPanel3.SetActive(true);
+    }
+
+    public void ShowPost4()
+    {
+        postPanel4.SetActive(true);
+    }
+
+    public void HidePost1()
+    {
+        postPanel1.SetActive(false);
+    }
 
     public void HidePost2()
     {
         postPanel2.SetActive(false);
+    }
+
+    public void HidePost3()
+    {
+        postPanel3.SetActive(false);
+    }
+
+    public void HidePost4()
+    {
+        postPanel4.SetActive(false);
     }
 }
