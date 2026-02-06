f6948e0da5fc29c769bd1b86de80fb8809d9e797
GruberWilfried <wilfried.gruber@htl-salzburg.ac.at>
2026-02-06 22:36:33 +0100

    Rework Scene 1LevelPasswort visually

--- Assets/MyGame/Scripts/PostManager.cs
+++ Assets/MyGame/Scripts/PostManager.cs
@@ -2,29 +2,6 @@ using UnityEngine;
 
 public class PostManager : MonoBehaviour
 {
-    public GameObject[] postPanels; //#18, #19
+    public GameObject postFeed;
 
-    void Start()
-    {
-        HideAllPosts();
-    }
-
-    public void ReturnButton()
-    {
-        HideAllPosts();
-    }
-
-    void HideAllPosts()
-    {
-        for (int i = 0; i < postPanels.Length; i++)
-        {
-            postPanels[i].SetActive(false);
-        }
-    }
-
-    public void ShowPost(int postNumber)
-    {
-        HideAllPosts();
-        postPanels[postNumber].SetActive(true);
-    }
 }
